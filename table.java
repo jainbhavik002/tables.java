417bbca3a6a8817bfaaa5f753594853308f1554d
Bhavik Jain <jainbhavik002@gmail.com>
2025-11-15 15:13:42 +0530

    Update table.java

--- table.java
+++ table.java
@@ -1,12 +1,14 @@
-import java.util.Scanner;  // Import the Scanner class
-
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("Enter username");
+    Scanner s = new Scanner(System.in);  
+    System.out.print("Enter a number to print its table: ");
+    int number = s.nextInt();  
 
-    String userName = myObj.nextLine();  // Read user input
-    System.out.println("Username is: " + userName);  // Output user input
+    for (int i = 1; i <= 10; i++) {  
+      System.out.println(number + " x " + i + " = " + (number * i));
+    }
+
+    s.close();  
   }
 }
-
